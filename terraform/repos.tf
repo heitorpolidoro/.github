fb543a0231561dda8706812f6b6f23ecff2003b6
Heitor Polidoro <heitor.polidoro@gmail.com>
2026-05-12 11:39:37 -0300

    chore: add DeepSource checks to required status checks in terraform

--- terraform/repos.tf
+++ terraform/repos.tf
@@ -6,18 +6,25 @@ locals {
   repos = {
     "cash_lens" = {
       extra_checks = [
-        { context = "build / Build and test", integration_id = 15368 }
+        { context = "build / Build and test", integration_id = 15368 },
+        { context = "DeepSource: Code Formatters" }
       ]
     }
     "meridian" = {
       extra_checks = [
-        { context = "build / Test (Node 24.x)", integration_id = 15368 }
+        { context = "build / Test (Node 24.x)", integration_id = 15368 },
+        { context = "DeepSource: JavaScript" },
+        { context = "DeepSource: Code Formatters" }
       ]
     }
     "sigecon" = {
       extra_checks = [
         { context = "Backend Tests" },
-        { context = "Frontend Tests" }
+        { context = "Frontend Tests" },
+        { context = "DeepSource: JavaScript" },
+        { context = "DeepSource: Python" },
+        { context = "DeepSource: Code Formatters" },
+        { context = "DeepSource: Docker" }
       ]
     }
   }
